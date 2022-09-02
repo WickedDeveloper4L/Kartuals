dc96a9de668dc83c0824ed3c5bcf3658199d4db5
unknown <wickeddeveloper4l@gmail.com>
2022-09-02 05:21:01 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component{
+  render(){
+    return (
+    <div>
+      Hello Papi
+    </div>
+  )
+  }
+  
+}
+
+export default App;
