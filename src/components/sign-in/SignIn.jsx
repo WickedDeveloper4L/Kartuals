8d1e249ebc283b945f1b4f59face1ad3c73017d5
unknown <wickeddeveloper4l@gmail.com>
2022-09-07 00:05:02 +0100

    added and initialized sign In and Sign Up components

--- src/components/sign-in/SignIn.jsx
+++ src/components/sign-in/SignIn.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import FormInput from '../form-input/FormInput';
 import './signIn.scss'
 import CustomButton from '../custom-button/CustomButton';
-import { signInWithGoogle } from '../firebase/firebase.utils';
+import {auth, signInWithGoogle } from '../firebase/firebase.utils';
 
 
 
@@ -16,13 +16,21 @@ export default class SignIn extends Component {
         }
     }
 
-    handleSubmit = event =>{
+    handleSubmit = async event =>{
         event.preventDefault()
 
-        this.setState({
+        const {email, password} = this.state;
+
+        try {
+          await auth.signInWithEmailAndPassword(email, password);
+          this.setState({
             email: '',
             password: ''
         })
+        } catch (error) {
+          console.log(error)
+        }
+        
     }
 
     handleChange = event =>{
